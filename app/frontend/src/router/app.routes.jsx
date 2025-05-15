2c7f1c1547f384d0efdea58731f0ab64822d851a
Eduardo-Malhao <eduardo.malhao@outlook.com>
2025-05-14 21:44:32 -0300

    feat/_login e seguranca

--- app/frontend/src/router/app.routes.jsx
+++ app/frontend/src/router/app.routes.jsx
@@ -1,52 +1,43 @@
-import { BrowserRouter, Route, Routes } from "react-router-dom";
-
+import { BrowserRouter, Route, Routes, Navigate } from "react-router-dom";
 import App from "../App";
 import HomeAdmin from "../pages/Admin/Home/home";
 import PublicRoute from "./public.routes";
+import PrivateRoutes from "./private.routes";
 import About from "../pages/Admin/About/About";
 import Profile from "../pages/Admin/Profile/Profile";
 import Products from "../pages/Admin/Products/Products";
 import Site from "../pages/Public/site";
+import Auth from "../pages/Public/Auth";
 
 export default function AppRoutes() {
   return (
     <BrowserRouter>
       <Routes>
-
-        <Route element={<App />}>
-
-        <Route path="/admin_home" element={
-          <PublicRoute>
-            <HomeAdmin />
-          </PublicRoute>
-        }/>
-
-<Route path="/admin_about" element={
-          <PublicRoute>
-            <About />
-          </PublicRoute>
-        }/>
-
-<Route path="/admin_profile" element={
-          <PublicRoute>
-            <Profile />
-          </PublicRoute>
-        }/>
-
-<Route path="/admin_products" element={
-          <PublicRoute>
-            <Products />
-          </PublicRoute>
-        }/>
-
-<Route path="/app" element={
+        {/* Rota pública - apenas /app */}
+        <Route path="/app" element={
           <PublicRoute>
             <Site />
           </PublicRoute>
-        }/>
+        } />
 
+        <Route path="/login" element={
+          <PublicRoute>
+            <Auth />
+          </PublicRoute>
+        } />
 
+        {/* Rotas privadas - agrupadas */}
+        <Route element={<PrivateRoutes />}>
+          <Route element={<App />}>
+            <Route path="/admin_home" element={<HomeAdmin />} />
+            <Route path="/admin_about" element={<About />} />
+            <Route path="/admin_profile" element={<Profile />} />
+            <Route path="/admin_products" element={<Products />} />
+          </Route>
         </Route>
+
+        {/* Redirecionamento para /app como fallback */}
+        <Route path="*" element={<Navigate to="/app" />} />
       </Routes>
     </BrowserRouter>
   );
